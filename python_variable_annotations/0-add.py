0b90d16fbeea3fea25ddd781f395358a641508ff
maram-ra <maramraddad33@gmail.com>
2025-07-31 04:29:22 +0300

    0-add.py

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that defines a function to add two floats."""
+
+
+def add(a: float, b: float) -> float:
+    """Returns the sum of a and b as a float."""
+    return a + b
