ae38475c70682a4c388bd21a65acc6684d4eb1e8
maram-ra <maramraddad33@gmail.com>
2025-08-14 18:11:41 +0300

    10-update_topics.py

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""define a function update_topics"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """update all documents"""
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
+    
